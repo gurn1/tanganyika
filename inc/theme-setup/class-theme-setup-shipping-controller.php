9b6e3840b02db6de77afb840b0ace8a306dfda93
Luke Clifton <luke@lscwebdesign.co.uk>
2022-04-05 17:08:51 +0100

    Added table rate shipping data

--- inc/theme-setup/class-theme-setup-shipping-controller.php
+++ inc/theme-setup/class-theme-setup-shipping-controller.php
@@ -22,8 +22,12 @@ if( ! class_exists('Raven_theme_setup_shipping_controller') ) {
 		function __construct() {
 
 			add_action( 'admin_init', array($this, 'setup'), 10);
+
 			// Woocommerce settings
 			//add_action( 'woocommerce_activated_plugin', array( $this, 'setup') );
+
+			//add_action( 'admin_init', array($this, 'update_current_instance'));
+
 		}
 		
 		/**
@@ -85,11 +89,11 @@ if( ! class_exists('Raven_theme_setup_shipping_controller') ) {
 		 * @since 1.0.0
 		 */
 		public function setup() {
-			if( WC_Shipping_Zones::get_zones() ) {
-				return;
-			}
-
 			if( class_exists( 'woocommerce' ) ) {
+				if( WC_Shipping_Zones::get_zones() ) {
+					return;
+				}
+
 				foreach( $this->shipping_zones() as $region => $countries ) {
 					if( ! in_array($region, $this->current_zones()) ) {
 						$zone_object = new WC_Shipping_Zone();
@@ -99,15 +103,138 @@ if( ! class_exists('Raven_theme_setup_shipping_controller') ) {
 							$zone_object->add_location( $country, 'country' );
 						}
 
-						$zone_object->save();
+						$zone_id = $zone_object->save();
 
-						if( class_exists('WC_RoyalMail') ) {
-							$zone_object->add_shipping_method('royal_mail');
-						}	
+						if( class_exists( 'WC_Table_Rate_Shipping' ) ) {
+							$instance_id = $zone_object->add_shipping_method('table_rate');
+							
+							$this->table_rate_shipping_data($instance_id);
+						}
+
 					}
 				}
 			}
-		} 
+		}
+
+		/**
+		 * Update current instance 
+		 * 
+		 * @since 1.0.0
+		 */
+		public function update_current_instance() {
+			$current_instance = isset($_GET['instance_id']) ? $_GET['instance_id'] : 0;
+			$update = isset($_GET['raven-update-instance']) ? $_GET['raven-update-instance'] : false;
+			
+			if( (isset($_GET['page']) && $_GET['page'] == 'wc-settings') && (isset($_GET['tab']) && ($_GET['tab'] == 'shipping') && $current_instance > 0) && $update == true ) {
+				$this->table_rate_shipping_data($current_instance);
+			}
+		}
+
+		/**
+		 * Add table data for table rate shipping
+		 * 
+		 * @since 1.0.0
+		 */
+		public function table_rate_shipping_data($shipping_method_id) {
+			if( class_exists( 'WC_Table_Rate_Shipping' ) && class_exists( 'woocommerce' ) ) {
+				global $wpdb;
+		
+				if( $shipping_method_id == 0 ) {
+					return;
+				}
+				
+				$data = array(
+					array( 'rate_condition' => 'Weight', 'rate_min' => 0, 'rate_max' =>  0.1 ),
+					array( 'rate_condition' => 'Weight', 'rate_min' => 0.101, 'rate_max' =>  0.250 ),
+					array( 'rate_condition' => 'Weight', 'rate_min' => 0.251, 'rate_max' =>  0.500 ),
+					array( 'rate_condition' => 'Weight', 'rate_min' => 0.501, 'rate_max' =>  0.750 ),
+					array( 'rate_condition' => 'Weight', 'rate_min' => 0.751, 'rate_max' =>  1 ),
+					//
+					array( 'rate_condition' => 'Weight', 'rate_min' => 1.001, 'rate_max' =>  1.250 ),
+					array( 'rate_condition' => 'Weight', 'rate_min' => 1.251, 'rate_max' =>  1.500 ),
+					array( 'rate_condition' => 'Weight', 'rate_min' => 1.501, 'rate_max' =>  1.750 ),
+					array( 'rate_condition' => 'Weight', 'rate_min' => 1.751, 'rate_max' =>  2 ),
+					//
+					array( 'rate_condition' => 'Weight', 'rate_min' => 2.001, 'rate_max' =>  2.250 ),
+					array( 'rate_condition' => 'Weight', 'rate_min' => 2.251, 'rate_max' =>  2.500 ),
+					array( 'rate_condition' => 'Weight', 'rate_min' => 2.501, 'rate_max' =>  2.750 ),
+					array( 'rate_condition' => 'Weight', 'rate_min' => 2.751, 'rate_max' =>  3 ),
+					//
+					array( 'rate_condition' => 'Weight', 'rate_min' => 3.001, 'rate_max' =>  3.250 ),
+					array( 'rate_condition' => 'Weight', 'rate_min' => 3.251, 'rate_max' =>  3.500 ),
+					array( 'rate_condition' => 'Weight', 'rate_min' => 3.501, 'rate_max' =>  3.750 ),
+					array( 'rate_condition' => 'Weight', 'rate_min' => 3.751, 'rate_max' =>  4 ),
+					//
+					array( 'rate_condition' => 'Weight', 'rate_min' => 4.001, 'rate_max' =>  4.250 ),
+					array( 'rate_condition' => 'Weight', 'rate_min' => 4.251, 'rate_max' =>  4.500 ),
+					array( 'rate_condition' => 'Weight', 'rate_min' => 4.501, 'rate_max' =>  4.750 ),
+					array( 'rate_condition' => 'Weight', 'rate_min' => 4.751, 'rate_max' =>  5 ),
+					//
+					array( 'rate_condition' => 'Weight', 'rate_min' => 5.001, 'rate_max' =>  5.500 ),
+					array( 'rate_condition' => 'Weight', 'rate_min' => 5.501, 'rate_max' =>  6 ),
+					//
+					array( 'rate_condition' => 'Weight', 'rate_min' => 6.001, 'rate_max' =>  6.500 ),
+					array( 'rate_condition' => 'Weight', 'rate_min' => 6.501, 'rate_max' =>  7 ),
+					//
+					array( 'rate_condition' => 'Weight', 'rate_min' => 7.001, 'rate_max' =>  7.500 ),
+					array( 'rate_condition' => 'Weight', 'rate_min' => 7.501, 'rate_max' =>  8 ),
+					//
+					array( 'rate_condition' => 'Weight', 'rate_min' => 8.001, 'rate_max' =>  8.500 ),
+					array( 'rate_condition' => 'Weight', 'rate_min' => 8.501, 'rate_max' =>  9 ),
+					//
+					array( 'rate_condition' => 'Weight', 'rate_min' => 9.001, 'rate_max' =>  9.500 ),
+					array( 'rate_condition' => 'Weight', 'rate_min' => 9.501, 'rate_max' =>  10 ),
+					//
+					array( 'rate_condition' => 'Weight', 'rate_min' => 10.001, 'rate_max' =>  15 ),
+					array( 'rate_condition' => 'Weight', 'rate_min' => 15.001, 'rate_max' =>  20 ),
+				);
+
+				$max_key = count($data) - 1;
+
+				for( $i = 0; $i <= $max_key; $i++ ) {
+					$rate_condition = $data[$i]['rate_condition'];
+					$rate_min = $data[$i]['rate_min'];
+					$rate_max = $data[$i]['rate_max'];
+
+					$result = $wpdb->insert(
+						$wpdb->prefix . 'woocommerce_shipping_table_rates',
+						array(
+							'rate_condition'            => sanitize_title( $rate_condition ),
+							'rate_min'                  => $rate_min,
+							'rate_max'                  => $rate_max,
+							'rate_cost'                	=> 0,
+							'rate_cost_per_item'        => 0,
+							'rate_cost_per_weight_unit' => 0,
+							'rate_cost_percent'         => 0,
+							'rate_label'                => '',
+							'rate_priority'             => 0,
+							'rate_order'                => $i,
+							'shipping_method_id'        => $shipping_method_id,
+							'rate_abort'                => 0,
+							'rate_abort_reason'         => 0,
+						),
+						array(
+							'%s',
+							'%s',
+							'%s',
+							'%s',
+							'%s',
+							'%s',
+							'%s',
+							'%s',
+							'%s',
+							'%s',
+							'%d',
+							'%d',
+							'%d',
+							'%s',
+						)
+					);
+				}
+			}
+
+
+		}
 		
 	}
 	
